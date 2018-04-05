7b7d2c5a2bbae9fe2c9ce01121e58f34c0bb4cd5
Joseph <jmgrant1995@gmail.com>
2018-04-06 01:02:59 +0800

    fix image not found issue....again

--- src/components/ImageContainer.js
+++ src/components/ImageContainer.js
@@ -7,20 +7,19 @@ import Image from './Image';
 import React from 'react';
 
 class ImageContainer extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
       photos: [],
-      loading: true,
+      loading: true
     };
   }
 
-  // Fetch images from API and set state
- /**
+  /**
    * Fetches images from API and sets state for the component
    * @param query
    */
-  showContent = (query) => {
+  loadContent = (query) => {
     this.setState({ loading: true });
 
     return FetchImages(query)
@@ -31,57 +30,59 @@ class ImageContainer extends React.Component {
           loading: false
         });
       })
-    }
-    
-    // called on site open
-    componentDidMount = () => {
-      this.showContent(this.props.match.params.query);
-    }
+  }
 
-    // called to update images
-    componentWillReceiveProps = (nextProps) => {
-      const currentQuery = this.props.match.params.query;
-      const nextQuery = nextProps.match.params.query;
+  // Called on inital launch
+  componentDidMount = () => {
+    this.loadContent(this.props.match.params.query);
+  }
 
-      if (currentQuery !== nextQuery) {
-        this.showContent(nextQuery);
-      }
-    }
+  // Called when the route path changes, so we can
+  // update the images
+  componentWillReceiveProps = (nextProps) => {
+    const currentQuery = this.props.match.params.query;
+    const nextQuery = nextProps.match.params.query;
 
-    render() {
-      const query = Capitalize(this.props.match.params.query);
-      const photos = this.state.photos;
-      let heading = null;
-
-      if (this.state.hasPhotos || this.state.loading) {
-        heading = 
-          <div>
-            <h1 className="image-title">Now showing images of: {query}</h1>
-          </div>
-
-        return (
-          <div>
-            {heading}
-            {
-              (this.state.loading)
-                ? <div className='loading'> </div>
-                :
-                <div className="image-container">
-                  {
-                    (this.state.hasPhotos)
-                      ? photos.map((photo) => {
-                        return (
-                          <Image id={photo.id} url={photo.url} source={photo.source} key={photo.id} />
-                        );
-                      })
-                      : <ImageNotFound />
-                  }
-                </div>
-            }
-          </div>
-        );
-      }
+    if (currentQuery !== nextQuery) {
+      this.loadContent(nextQuery);
     }
   }
+
+  render() {
+    const query = Capitalize(this.props.match.params.query);
+    const photos = this.state.photos;
+    let heading = null;
+
+    if (this.state.hasPhotos || this.state.loading) {
+      heading =
+        <div>
+          <h1 className='image-title'>Now showing images of: {query}</h1>
+        </div>;
+    }
+
+    return (
+      <div>
+        {heading}
+        {
+          (this.state.loading)
+            ? <div className='loading'> </div>
+            :
+            <div>
+              {
+                (this.state.hasPhotos)
+                  ? photos.map((photo) => {
+                    return (
+                      <Image id={photo.id} url={photo.url} source={photo.source} key={photo.id} />
+                    );
+                  })
+                  : <ImageNotFound />
+              }
+            </div>
+        }
+      </div>
+    );
+  }
+}
+
 
 export default ImageContainer;
