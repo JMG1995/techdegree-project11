eeb2d9ff5ae569d8ebe690b0065816ca6b391602
Joseph <jmgrant1995@gmail.com>
2018-05-04 22:49:55 +0800

    add hashrouter, delete service workers

--- src/index.js
+++ src/index.js
@@ -2,12 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { HashRouter, Route } from 'react-router-dom';
 
 ReactDOM.render((
-  <BrowserRouter>
+  <HashRouter>
     <Route component={App} />
-  </BrowserRouter>),
+  </HashRouter>),
   document.getElementById('root'));
-registerServiceWorker();
